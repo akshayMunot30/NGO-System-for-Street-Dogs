026941f60ef5701a051606215c653339a38d20a0
SahilHemnani777 <hemnanisahil777@gmail.com>
2021-11-23 19:12:02 +0530

    setup alfred

--- server/bin/dart_application_1.dart
+++ server/bin/dart_application_1.dart
@@ -1,5 +1,24 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+import 'package:alfred/alfred.dart';
+
+void main(List<String> arguments) async{
+  final app = Alfred();
+
+  app.get('/sahil', (req, res) => 'Sahil is working on db with akshay');
+
+  app.get('/json', (req, res) => {'json_response': true});
+
+  app.get('/jsonExpressStyle', (req, res) {
+    res.json({'type': 'traditional_json_response'});
+  });
+
+  app.get('/file', (req, res) => File('test/files/image.jpg'));
+
+  app.get('/html', (req, res) {
+    res.headers.contentType = ContentType.html;
+    return '<html><body><h1>Test HTML</h1></body></html>';
+  });
+
+  await app.listen(6565); //Listening on port 6565
 }
