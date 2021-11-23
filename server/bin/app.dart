176909ee57dc722153b5204adc8e50d0ba83b86e
akshaymunot30 <akshaymunot30@gmail.com>
2021-11-23 19:50:39 +0530

    fdfsds

--- server/bin/app.dart
+++ server/bin/app.dart
@@ -1,13 +1,22 @@
 import 'package:alfred/alfred.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
-void main(List<String> arguments) async{
+
+
+void main(List<String> arguments) async {
   final app = Alfred();
+  final db = await Db.create(
+      'mongodb+srv://akshaymunot30:akshay@cluster0.ozpyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
-  app.get('/testDb', (req, res) async{
-    var data = 'dummy'; // come from mongo databasse
+  await db.open();
+  final coll = db.collection('test');
+
+  
+
+  app.get('/testDb', (req, res) async {
+    var data = await coll.find().toList().toString();
     return data;
   });
 
-  
-  await app.listen(6565); //Listening on port 6565
+  await app.listen(3001); //Listening on port 3001
 }
