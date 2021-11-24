d9af533f1950ded0b0c87bebbe27b8f59519e35d
AbdulMughni23 <79201304+AbdulMughni23@users.noreply.github.com>
2021-11-24 12:42:50 +0530

    removed errors

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
+
 import './screens/add_new_locality.dart';
-=======
 import 'package:ngo_system_for_street_dogs/api.dart';
->>>>>>> ed74b8869b2fe6f6ed5605a1e8faee7782b92f89
 
 import 'package:ngo_system_for_street_dogs/screens/adminLogin.dart';
 import 'package:ngo_system_for_street_dogs/screens/volunteerLogin.dart';
@@ -97,59 +95,55 @@ class _MyHomePageState extends State<MyHomePage> {
                       fontWeight: FontWeight.bold,
                       fontStyle: FontStyle.italic),
                 ),
-<<<<<<< HEAD
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AdminLogin(),
-                    ),
-                  );
-                },
-                child: Text("Login as Admin"),
-              ),
-              Divider(
-                indent: 110,
-                endIndent: 110,
-                height: 50,
-                thickness: 1,
-                color: Colors.black,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VolunteerLogin(),
-                    ),
-                  );
-                },
-                child: Text("Login as Volunteer"),
-              ),
-              SizedBox(
-                height: 150,
-              ),
-              Text(
-                "\"DOGS DO SPEAK,\nBUT ONLY TO THOSE,\nWHO KNOW HOW TO LISTEN\"",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.black54,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic),
-              ),
-              // ElevatedButton(onPressed: ()=>{}), child: Text("Login")),
-            ],
-=======
+
+                SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AdminLogin(),
+                      ),
+                    );
+                  },
+                  child: Text("Login as Admin"),
+                ),
+                Divider(
+                  indent: 110,
+                  endIndent: 110,
+                  height: 50,
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => VolunteerLogin(),
+                      ),
+                    );
+                  },
+                  child: Text("Login as Volunteer"),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Text(
+                  "\"DOGS DO SPEAK,\nBUT ONLY TO THOSE,\nWHO KNOW HOW TO LISTEN\"",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+                // ElevatedButton(onPressed: ()=>{}), child: Text("Login")),
 
                 // ElevatedButton(onPressed: ()=>{}), child: Text("Login")),
               ],
             ),
->>>>>>> ed74b8869b2fe6f6ed5605a1e8faee7782b92f89
           ),
         ),
       ),
